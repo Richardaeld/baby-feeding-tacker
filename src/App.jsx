1aa4cd20d6920f71e0ed02efa423dd6c3cbf07d3
Richard E. <richardaeld@gmail.com>
2025-01-26 18:41:00 +0000

    Added basic form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
 // import { useState } from 'react'
 
-function App() {
+export function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Baby Feeding Tracker</h1>
+      <div>
+        <h2>Babies</h2>
+        <div>
+          <h3>baby name</h3>
+        </div>
+        <div>
+          <h3>baby name</h3>
+        </div>
+      </div>
+
+    <form action="">
+      <h2>Add Feeding</h2>
+      <label htmlFor=""> 
+        name
+        <input type="text" />
+      </label>
+      <label htmlFor=""> 
+        time
+        <input type="text" />
+      </label>
+      <label htmlFor=""> 
+        consumed
+        <input type="text" />
+      </label>
+      <label htmlFor=""> 
+        extra hungry
+        <input type="text" />
+      </label>
+      <label htmlFor=""> 
+        name
+        <input type="text" />
+      </label>
+      <label htmlFor=""> 
+        name
+        <input type="text" />
+      </label>
+      <label htmlFor=""> 
+        Notes
+        <textarea name="" id=""></textarea>
+      </label>
+      <button>submit</button>
+    </form>
+
+    </main>
   )
 }
 
-export default App
