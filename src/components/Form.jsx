55c9402a4f77fb1eb2b0c7e7c65e884782873ac5
Richard E. <richardaeld@gmail.com>
2025-02-02 20:04:49 +0000

    updated/added add/remove class functions, added Form.css, finished creation of input_radio

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import '../css/form.css';
+
 import { FormInput } from './FormInput'
 import { FormTextarea } from './FormTextarea';
 import { FormCheckbox } from './FormCheckbox';
 import { FormSelect } from './FormSelect';
-import { useState } from 'react';
 import { FormRadio } from './FormRadio';
 
 export function Form ({ form, addFeeding, feeding }) {
@@ -16,34 +19,46 @@ export function Form ({ form, addFeeding, feeding }) {
       addFeeding(formData);
    };
 
-   const addClass = (addClass, e) =>  
-      e.target.closest('button').classList.add(addClass);
+   const addClass = (addClass, closestTag, e) =>
+      e.target.closest(closestTag).classList.add(addClass);
 
-   const removeClass = (removeClass, e) => 
-      e.target.closest('button').classList.remove(removeClass);
-   
+   const removeClass = (removeClass, closestTag, e) =>
+      e.target.closest(closestTag).classList.remove(removeClass);
+
+   const removeClassRadio = (removeClasses, closestTag, e) => {
+      const
+         radioContainer = e.target.closest("div.input-radio-buttons"),
+         radioButtons   = radioContainer.querySelectorAll('label');
+
+      radioButtons.forEach(button => {
+         removeClasses.map(removeClass => {
+            if (!button.querySelector('input').checked) button.classList.remove(removeClass)
+         })
+      })
+   }
+
 
    return (
       <form onSubmit={handleSubmit}>
          <h2>{form.header}</h2>
          <div>
             {form.inputs.map(input => {
-               // console.log(input.type)
+               // console.log(input)
                switch (input.type) {
                   case 'textarea'      : return <FormTextarea key={input.key} inputData={input} setFormData={setFormData}/>
                   case 'input_checkbox': return <FormCheckbox key={input.key} inputData={input} setFormData={setFormData}/>
                   case 'input'         :
                   case 'input_text'    : return <FormInput  key={input.key}   inputData={input} setFormData={setFormData}/>
                   case 'select'        : return <FormSelect key={input.key}   inputData={input} setFormData={setFormData} />
-                  // case 'input_radio'   : return <FormRadio  key={input.key} formInput={input} setFormData={setFormData} />
+                  case 'input_radio'   : return <FormRadio  key={input.key}   inputData={input} setFormData={setFormData} addClass={addClass} removeClass={removeClassRadio} />
                }
             })}
          </div>
-         <div>
-            <button 
-               onMouseEnter={(e) => addClass('button-depressed', e)} 
-               onMouseLeave={(e) => removeClass('button-depressed', e)} 
-               className="button submit-button"
+         <div className="submit-div">
+            <button
+               onMouseEnter={(e) =>    addClass('button-depressed','button', e)}
+               onMouseLeave={(e) => removeClass('button-depressed','button', e)}
+               className="button button-clickable"
             >
                <div>Submit</div>
             </button>
