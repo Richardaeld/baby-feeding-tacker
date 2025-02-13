290dda55661d6c7bcee6debd68d50433fc65eab6
Richard E. <richardaeld@gmail.com>
2025-02-13 16:46:00 +0000

    added fontawesome icons

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -4,16 +4,56 @@ import { format } from 'date-fns';
 export function EventItem({ event }) {
    // console.log('EventItem.jsx',event)
 
-   const fontAwesomeMap = {
+{/* <i class="fa-solid fa-circle-plus"></i> -- add */}
 
+   const fontAwesomeMap = {
+      BATH: [
+         "fa-solid fa-bath",
+         "fa-solid fa-shower",
+      ],
+      DIAPER: [
+         "fa-solid fa-baby",
+         "fa-solid fa-toilet",
+         "fa-solid fa-toilet-paper",
+      ],
+      FEEDING:[
+         "fa-solid fa-drumstick-bite",
+         // "fa-solid fa-bottle-water",
+         // "fa-solid fa-bottle-droplet",
+      ],
+      GROWTH: [
+         "fa-solid fa-tape",
+         "fa-solid fa-toilet-paper",
+      ],
+      MEDICATION:[
+         "fa-solid fa-stethoscope",
+         "fa-solid fa-pump-medical",
+         "fa-solid fa-tablets",
+         "fa-solid fa-prescription",
+         "fa-solid fa-pills",
+      ],
+      NIGHT_CHECK: [
+         "fa-regular fa-moon",
+      ],
+      PUMPING:[
+         "fa-solid fa-gas-pump",
+         "fa-solid fa-person-breastfeeding",
+      ],
+      TEMPERATURE:[
+         "fa-solid fa-temperature-three-quarters",
+      ],
+      TUMMY_TIME: [
+         "fa-solid fa-baby",
+      ],
    };
 
-
+   // console.log(event.event_type)
 
 
   return (
       <div className={`event-item ${event.event_type}`}>
          <div>
+            <div><i className={fontAwesomeMap[event.event_type]?.[0]}></i></div>
             {/* <div><b>{event.name}</b></div> */}
             <div><b>{event.first_name}</b></div>
             {/* <div>{format(event.start_on, 'MM/dd HH:mm')}</div> */}
