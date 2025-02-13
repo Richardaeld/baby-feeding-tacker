ae9ceb5dcb3ef15e0a3a1d72d116290aca18db38
Richard E. <richardaeld@gmail.com>
2025-02-13 15:39:17 +0000

    imporved lay out of Home to see if design will work

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -3,12 +3,21 @@ import { format } from 'date-fns';
 
 export function EventItem({ event }) {
    // console.log('EventItem.jsx',event)
+
+   const fontAwesomeMap = {
+
+   };
+
+
+
+
   return (
       <div className={`event-item ${event.event_type}`}>
          <div>
             {/* <div><b>{event.name}</b></div> */}
             <div><b>{event.first_name}</b></div>
-            <div>{format(event.start_on, 'MM/dd HH:mm')}</div>
+            {/* <div>{format(event.start_on, 'MM/dd HH:mm')}</div> */}
+            <div>{format(event.start_on, 'HH:mm')}</div>
          </div>
          {/* <div>
             <div>{event.feeding_type}</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,36 @@
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 
 import { EventItem } from '../components/EventItem'
 
 export function Home({ events }) {
 
    console.log('Home', events)
+   let date, time;
+
 
   return (
-    <section>
+    <section className="home">
       {events.map(event =>{
-         console.log({
-            Start: event.start_on,
-            End: event.end_on
-         })
+         const data = {
+            start_time: format(event.start_on, 'MM/dd'),
+            start_date: format(event.start_on, 'MM/dd'),
+            start_day:  format(event.start_on, 'EEE'),
+            end_time:   format(event.end_on  , 'HH:mm'),
+            end_date:   format(event.end_on  , 'HH:mm'),
+         }
+         console.log('EventItem', data)
+
+         if (!date || date !== data.start_date) {
+            date = data.start_date;
+            time = data.start_time;
+            return (
+               <>
+                  <div className="new-day"><b>{data.start_date} ({(data.start_day)})</b><div></div></div>
+                  <EventItem event={event}/>
+               </>
+            )
+         }
 
          return <EventItem event={event}/>
       })}
