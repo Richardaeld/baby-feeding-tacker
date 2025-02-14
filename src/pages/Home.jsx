d48b8c9972d0a3056ee9d7c7a7b63e864b26674d
Richard E. <richardaeld@gmail.com>
2025-02-14 01:33:03 +0000

    cleaned up Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,33 @@
-import PropTypes from 'prop-types';
+import PropTypes, { arrayOf } from 'prop-types';
 import { format } from 'date-fns';
 
-import { EventItem } from '../components/EventItem'
+// import { EventItem } from '../components/EventItem'
 import { EventBlock } from '../components/EventBlock';
 import { EventHeader } from '../components/EventHeader';
 
 export function Home({ events }) {
+   let
+      contentDay,
+      contentTime,
+      previousDay,
+      previousHour,
+      currentDay,
+      currentHour;
 
-   // console.log('Home', events)
-   let date, time, hour, minute, contentDay, contentTime;
-   const babies = [];
-   const splitEvents = {};
+   const
+      babies      = [],
+      splitEvents = {};
+
    events.forEach(event => {
       const date = format(event.start_on, "yyyy/MM/dd HH");
       if (!splitEvents[date]) splitEvents[date] = {};
       if (!splitEvents[date][event.baby_id]) {
          splitEvents[date][event.baby_id] = [];
          if (!babies.includes(event.first_name)) babies.unshift(event.first_name)
-
       }
       splitEvents[date][event.baby_id].push(event)
-
-// console.log(event)
-
    })
-let
-   previousDay,
-   previousHour,
-   currentDay,
-   currentHour;
-
    console.log(splitEvents)
-   // console.log(date)
 
   return (
     <section className="home">
@@ -70,5 +66,5 @@ let
 }
 
 Home.propTypes = {
-
+   events: PropTypes.arrayOf()
 };
