3151516b1b74440e55280563bdb45c507e23dd0a
Richard E. <richardaeld@gmail.com>
2025-02-14 20:03:17 +0000

    updated us of check boxes to be similar to radio buttons

--- src/components/FormCheckbox.jsx
+++ src/components/FormCheckbox.jsx
@@ -2,15 +2,32 @@ import PropTypes from 'prop-types';
 import { capitalizeEveryFirstLetter } from './../js/general.js';
 import { useState } from 'react';
 
-export function FormCheckbox({ inputData, setFormData }) {
+export function FormCheckbox({ inputData, setFormData, addClass, removeClass}) {
    const [newValue, setNewValue] = useState("");
 
-  return (
-   <fieldset className={inputData.className}>
-      <span>{capitalizeEveryFirstLetter(inputData.name)}</span>
-      <label htmlFor={inputData.name}>
+   const toggleInput = (toggleClass, closestTag, e) => {
+      requestAnimationFrame(() => {
+         const target = e.target.closest(closestTag);
+         const bool = target.querySelector('input').checked
+         console.log(bool)
+         bool
+            ? target.classList.add(toggleClass)
+            : target.classList.remove(toggleClass);
 
-         <span>{capitalizeEveryFirstLetter(inputData.name)}</span>
+      })
+   }
+
+  return (
+   <fieldset className={`input-checkbox-buttons  ${inputData.className}`}>
+      <span style={{visibility:'hidden'}}>{capitalizeEveryFirstLetter(inputData.name)}</span>
+      <label
+         className={`button button-clickable`}
+         htmlFor={inputData.name}
+         onMouseEnter={(e) =>    addClass('button-depressed','label', e)}
+         onMouseLeave={(e) => removeClass(['button-depressed','input-radio-selected'],'label', 'fieldset.input-checkbox-buttons', e)}
+         onClick={(e)      => toggleInput('input-radio-selected', 'label', e)}
+      >
+         {/* <span>{capitalizeEveryFirstLetter(inputData.name)}</span> */}
          <input
             id={inputData.name}
             type="checkbox"
@@ -24,6 +41,7 @@ export function FormCheckbox({ inputData, setFormData }) {
             }}
             checked={newValue}
          />
+         <p>{capitalizeEveryFirstLetter(inputData.name)}</p>
       </label>
    </fieldset>
 
