cf384b37b09b42da2e92a4b2bd885a4b1d6af32f
Richard E. <richardaeld@gmail.com>
2025-02-15 16:58:42 +0000

    added ability to save form popup

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,14 +4,14 @@ import '../css/form.css';
 
 import { Form } from '../components/Form';
 
-export function Modal({ modalChildren, isModalOpen, closeModal, children, form, addEvent, events, redirectHome, setRedirectHome }) {
+export function Modal({ modalChildren, isModalOpen, closeModal, children, form, addEvent, setEvents, events, redirectHome, setRedirectHome }) {
    if (!isModalOpen) return null;
 
    return (
       <div className="modal-overlay" onClick={closeModal}>
         <div className="modal" onClick={(e) => e.stopPropagation()}>
           <button className="close-button" onClick={closeModal}>X</button>
-          {<Form form={form} addEvent={addEvent} feeding={events} isModalOpen={isModalOpen} redirectHome={redirectHome} setRedirectHome={setRedirectHome} closeModal={closeModal}/>}
+          {<Form form={form} addEvent={addEvent} setEvents={setEvents} feeding={events} isModalOpen={isModalOpen} redirectHome={redirectHome} setRedirectHome={setRedirectHome} closeModal={closeModal}/>}
         </div>
       </div>
    );
